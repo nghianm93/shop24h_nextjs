9052ab1d23477e72c4ebb7cbec1414c57a6d0869
nghianm2 <nghianm@devcamp.edu.vn>
2022-07-08 22:57:59 +0700

    .env initial commit

--- shop24h/lib/client.js
+++ shop24h/lib/client.js
@@ -2,9 +2,9 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: 'vfxfwnaw',
+  projectId: 'hd0pknu1',
   dataset: 'production',
-  apiVersion: '2022-03-10',
+  apiVersion: '2022-07-05',
   useCdn: true,
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN
 });
