98779ee41686cff3d3e46951f8f90870f1c309e4
nghianm2 <nghianm@devcamp.edu.vn>
2022-07-08 22:04:01 +0700

    production commit

--- shop24h/components/Layout.jsx
+++ shop24h/components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>Shop24h</title>
       </Head>
       <header>
         <Navbar />
